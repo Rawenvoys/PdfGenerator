b5837880347aa0ca51f16c9cc05be27b235c4a58
AIGCRDEV\lwisniewski <lukasz.wisniewski@santanderconsumer.pl>
2024-08-27 11:49:22 +0200

    MediatR

---
+++ Request.Infrastracture/Services/RequestService.cs
@@ -0,0 +1,21 @@
+﻿using PdfGenerator.Request.Domain.IServices;
+using PdfGenerator.Request.Domain.Models;
+using PdfGenerator.Request.Infrastracture.IRepositories;
+using PdfGenerator.Template.Domain.IServices;
+
+namespace PdfGenerator.Request.Infrastracture.Services;
+internal class RequestService(ITemplateService templateService, IRequestRepository requestRepository) : IRequestService
+{
+    private readonly ITemplateService _templateService = templateService;
+    private readonly IRequestRepository _requestRepository = requestRepository;
+    //private readonly IDataService _dataService = dataService;
+
+    public async Task<Guid> Save(string templateName, string xmlData)
+    {
+        var templateModel = await _templateService.GetAsync(templateName);
+        var requestId = await _requestRepository.SaveAsync(templateModel.Id);
+        //await _dataService.SaveAsync(xmlData);
+
+        return requestId;
+    }
+}
