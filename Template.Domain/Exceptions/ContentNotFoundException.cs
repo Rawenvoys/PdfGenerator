8c4c6277ef315da28c6619f119333257a24fdb68
AIGCRDEV\lwisniewski <lukasz.wisniewski@santanderconsumer.pl>
2024-08-28 09:19:59 +0200

    Throw ContentNotFoundException when File not exist

---
+++ Template.Domain/Exceptions/ContentNotFoundException.cs
@@ -0,0 +1,4 @@
+﻿namespace PdfGenerator.Template.Domain.Exceptions;
+public class ContentNotFoundException(string fileStoragePath) : FileNotFoundException($"Unable to find the specified file in path {fileStoragePath}")
+{
+}
--- Template.Infrastracture/Finders/ContentFinder.cs
+++ Template.Infrastracture/Finders/ContentFinder.cs
@@ -1,8 +1,12 @@
-﻿using PdfGenerator.Template.Infrastracture.IFinders;
+﻿using PdfGenerator.Template.Domain.Exceptions;
+using PdfGenerator.Template.Infrastracture.IFinders;
 
 namespace PdfGenerator.Template.Infrastracture.Finders;
 internal class ContentFinder : IContentFinder
 {
-    public string Find(string fileStoragePath) 
-        => File.ReadAllText(fileStoragePath);
+    public string Find(string fileStoragePath)
+    {
+        if (!File.Exists(fileStoragePath)) throw new ContentNotFoundException(fileStoragePath);
+        return File.ReadAllText(fileStoragePath);
+    }
 }
