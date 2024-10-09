2a22e46b3ca25f96c4773541e36732e49b21c0f3
Daniel.Bednarz <Daniel.Bednarz@santanderconsumer.pl>
2024-10-09 13:44:15 +0200

    implement StatusModel

---
+++ Microservices/User/User.Domain/IRepositories/IStatusRepository.cs
@@ -0,0 +1,7 @@
+﻿using PdfGenerator.User.Domain.Enums;
+
+namespace PdfGenerator.User.Domain.IRepositories;
+internal interface IStatusRepository
+{
+    Task SetAsync(Guid userId, Status status);
+}
